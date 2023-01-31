4e0371932d03c57528b4c278a7471e87a53e0069
Ivan Sedelkin <ivan.sedelkin9@gmail.com>
2023-01-31 19:47:51 +0100

    created a button with events for both hold and release

--- app/src/main/java/com/example/mimic/MainActivity.java
+++ app/src/main/java/com/example/mimic/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mimic;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.QiSDK;
@@ -14,11 +17,14 @@ import com.aldebaran.qi.sdk.design.activity.RobotActivity;
 
 
 public class MainActivity extends RobotActivity implements RobotLifecycleCallbacks{
+    ImageButton speak_button;
+    private boolean isSpeakButtonLongPressed = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Register the RobotLifecycleCallbacks to this Activity.
         QiSDK.register(this, this);
+        setContentView(R.layout.layout);
     }
 
     @Override
@@ -31,12 +37,16 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
     @Override
     public void onRobotFocusGained(QiContext qiContext) {
         // The robot focus is gained.
-        Say say = SayBuilder.with(qiContext) // Create the builder with the context.
+        /*Say say = SayBuilder.with(qiContext) // Create the builder with the context.
                 .withText("Stop stealing code Oscar!") // Set the text to say.
                 .build(); // Build the say action.
 
         // Execute the action.
-        say.run();
+        say.run();*/
+        this.speak_button = findViewById(R.id.pepper_talk);
+        this.speak_button.setOnLongClickListener(speakHoldListener);
+        this.speak_button.setOnTouchListener(speakTouchListener);
+
     }
 
     @Override
@@ -48,4 +58,32 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
     public void onRobotFocusRefused(String reason) {
         // The robot focus is refused.
     }
+
+    // Here lies our logic for when the button is held down
+    private View.OnLongClickListener speakHoldListener = new View.OnLongClickListener() {
+
+        @Override
+        public boolean onLongClick(View pView) {
+            // Do something when your hold starts here.
+            isSpeakButtonLongPressed = true;
+            return true;
+        }
+    };
+
+    // Here lies our logic for when the button is released
+    private View.OnTouchListener speakTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            view.onTouchEvent(motionEvent);
+            // We're only interested in when the button is released.
+            if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                // We're only interested in anything if our speak button is currently pressed.
+                if (isSpeakButtonLongPressed) {
+                    // Do something when the button is released.
+                    isSpeakButtonLongPressed = false;
+                }
+            }
+            return false;
+        }
+    };
 }
