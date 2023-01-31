4ea4a102712ee30e091f1cfd5e6b046a51b52c49
Epos95 <39768937+Epos95@users.noreply.github.com>
2023-01-31 11:15:18 +0100

    added comments

--- app/src/main/java/com/example/mimic/ActionBuilder.java
+++ app/src/main/java/com/example/mimic/ActionBuilder.java
@@ -5,11 +5,19 @@ public class ActionBuilder {
     private String actionAnimationName;
     private boolean actionIsAsync;
 
+    /**
+     * @param value whether to use async
+     * @return The ActionBuilder so that it can be reused
+     */
     public ActionBuilder is_async(boolean value) {
         this.actionIsAsync = value;
         return this;
     }
 
+    /**
+     * @param value The name of the animation to use for the Action
+     * @return The ActionBuilder so that it can be reused
+     */
     public ActionBuilder animation_name(String value) {
         this.actionAnimationName = value;
         return this;
--- app/src/main/java/com/example/mimic/ActionProcessor.java
+++ app/src/main/java/com/example/mimic/ActionProcessor.java
@@ -4,7 +4,6 @@ package com.example.mimic;
 import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.object.actuation.Animation;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -12,11 +11,10 @@ import java.util.HashMap;
 // Since using animations needs the specific files it makes sense to convert the `Action` into a
 // pepper friendly executable object
 public class ActionProcessor {
-    // Since we only need one actionprocessor throughout the entire application (i think)
+    // Since we only need one action processor throughout the entire application (i think)
     // maybe make this static or use singleton.
 
     private static ActionProcessor instance = null;
-
 
 
     // The queued actions to use for the next run.
@@ -32,7 +30,11 @@ public class ActionProcessor {
         this.ctx = ctx;
     }
 
-    // Adds a action to be executed later
+    /**
+     * Adds a action to be executed by the ActionProcessor later.
+     *
+     * @param action The Action to execute
+     */
     public void queueAction(Action action) {
         this.queued_actions.add(action);
     }
@@ -48,11 +50,23 @@ public class ActionProcessor {
         animations.put(animationName, animation);
     }
 
+    /**
+     * Deletes the Action at the specified index from the execution queue.
+     * @param index The index of the Action to delete.
+     * @return The deleted Action or null if none is deleted.
+     */
     public Action deleteAction(int index) {
         return this.queued_actions.remove(index);
     }
 
+    /**
+     * Deletes the Action with the specified name from the execution queue.
+     *
+     * @param name The name of the action to delete from the queue
+     * @return The deleted Action or null if none is deleted.
+     */
     public Action deleteAction(String name) {
+        // TODO: What if there are two actions in the queue with the same name and we want the latter to be removed?
         for (Action action :
                 this.queued_actions) {
             if (action.getName().equals(name)) {
@@ -63,6 +77,12 @@ public class ActionProcessor {
         return null;
     }
 
+    /**
+     * Searches the ActionProcessor's stored animations to convert a string to a Animation object.
+     *
+     * @param name The name of the animation to search for
+     * @return Returns the animation object (or null)
+     */
     public Animation getAnimation(String name) {
         return this.animations.get(name);
     }
@@ -77,6 +97,11 @@ public class ActionProcessor {
         return instance;
     }
 
+    /**
+     * Executes the queued actions.
+     *
+     * @param asAsync Whether or not to execute the queue in a asynchronous manner.
+     */
     public void executeQueue(boolean asAsync) {
         // This execution requires some object *like* the robot context to actually execute on, keep this internally in the action processor in that case.
         for (Action action :
