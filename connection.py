1f4bd045c6ada287d868c20d74f8ab5234836f89
Epos95 <39768937+Epos95@users.noreply.github.com>
2023-03-19 13:50:21 +0100

    added a bit of documentation

--- connection.py
+++ connection.py
@@ -4,6 +4,10 @@ import sys
 import os
 
 class Connection():
+    """
+    A connection to a pepper robot.
+    Reads the necessary information to create a connection from environment variables and then gives seamless access to the Pepper's services.
+    """
     def __init__(self):
         self.ip = os.getenv("PEPPER_IP")
         self.port = os.getenv("PEPPER_PORT")
@@ -24,6 +28,12 @@ class Connection():
     # which makes configuration easier.
     # Returns None if the module is not found.
     def get_service(self, name):
+        """
+        Gets a service object connected to the Pepper robot.
+
+        All retrieved services gets stored in a cache, this lets us share services between all "modules" which makes configuration easier.
+        Returns None if the module is not found.
+        """
         if name not in self.services:
             print("Creating new instance of service and adding to cache")
 
